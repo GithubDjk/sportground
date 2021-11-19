e51abe62d81efa6949c8bca055719fe39f67ab48
GithubDjk <dipendrajungkarki@bajratechnologies.com>
2021-11-19 13:03:00 +0545

    feat : sweet-alert-added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,9 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+import Swal from 'sweetalert2/sweetalert2.js'
+import 'sweetalert2/src/sweetalert2.scss'
+window.Swal = Swal;
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
