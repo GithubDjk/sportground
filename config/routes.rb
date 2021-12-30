6542af6921118139fe7a1c5a823a9b24d7b7601d
bajraDjkGithub <dipendrajungkarki@bajratechnologies.com>
2021-12-30 21:34:16 +0545

    user_contact_and_futsal_field_enlarged

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :futsals
+  resources :settings, only: [:index, :create]
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "futsals#index" 
