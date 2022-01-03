e97075b9bc98e446b6d335d166f5b1e698deebea
bajraDjkGithub <dipendrajungkarki@bajratechnologies.com>
2022-01-03 21:09:17 +0545

    feat : approve_futsal added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_31_154101) do
+ActiveRecord::Schema.define(version: 2022_01_03_151020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2021_12_31_154101) do
     t.integer "capacity"
     t.bigint "contact_no"
     t.string "bio"
+    t.boolean "approve"
   end
 
   create_table "users", force: :cascade do |t|
