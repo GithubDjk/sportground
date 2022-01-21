f966fdd78636fffa1e7e3469e94d37bce4305ef2
bajraDjkGithub <dipendrajungkarki@bajratechnologies.com>
2022-01-21 22:27:03 +0545

    fix : config_added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,20 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  #devise_confirmable_production
+  config.action_mailer.default_url_options = { host => 'https://protected-shore-80321.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name:      ENV['GMAIL_PASSWORD'],
+    password:       ENV['GMAIL_USERNAME'],
+    domain:         'gmail.com',
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+  config.action_mailer.perform_deliveries = true
+
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
