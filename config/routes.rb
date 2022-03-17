0e2b6154861f83ab9bdbf712792b1f44801d6344
bajraDjkGithub <dipendrajungkarki@bajratechnologies.com>
2022-03-17 21:28:01 +0545

    Booking listed and About Us designed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :futsals
-  resources :booking, only: %i[create update destroy]
+  resources :booking
   resources :settings, only: %i[index create]
+  resources :contact, only: %i[index]
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "futsals#index" 
