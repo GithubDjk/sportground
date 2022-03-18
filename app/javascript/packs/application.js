8582367feb97665edf1cb9c9c7c99e55e48554d3
bajraDjkGithub <dipendrajungkarki@bajratechnologies.com>
2022-03-18 23:37:57 +0545

    fix: font-awsome from application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,6 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-import '../src/font-awesome.js';
 import Swal from 'sweetalert2/sweetalert2.js'
 import 'sweetalert2/src/sweetalert2.scss'
 window.Swal = Swal;
