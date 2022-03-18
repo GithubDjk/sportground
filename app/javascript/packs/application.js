1254e539738aa6ba61375cd33aeb0395cedcfba0
bajraDjkGithub <dipendrajungkarki@bajratechnologies.com>
2022-03-19 01:21:51 +0545

    fix : deploy

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-import Swal from 'sweetalert2/sweetalert2.js'
+// import Swal from 'sweetalert2/sweetalert2.js'
 import 'sweetalert2/src/sweetalert2.scss'
 window.Swal = Swal;
 import "../stylesheets/application"
