de2be500e9bc62ed3afcfb52798063805961d911
bajraDjkGithub <dipendrajungkarki@bajratechnologies.com>
2022-04-01 12:53:36 +0545

    fix : payment and design issue

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "futsals#index" 
+  post '/verify_payment', to: 'booking#payment'
 end
