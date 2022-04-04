1cc4562db34891673db20bfa96823e6a1d8fc10b
bajraDjkGithub <dipendrajungkarki@bajratechnologies.com>
2022-04-04 23:33:06 +0545

    fix : check column existance

--- db/migrate/20220404071459_add_and_remove_column_booking.rb
+++ db/migrate/20220404071459_add_and_remove_column_booking.rb
@@ -3,6 +3,6 @@ class AddAndRemoveColumnBooking < ActiveRecord::Migration[6.1]
     remove_column :bookings, :starts_at
     remove_column :bookings, :ends_at
     add_column :bookings, :book_date, :date
-    add_column :bookings, :book_time, :integer
+    add_column :bookings, :book_time, :integer , if_not_exists: true
   end
 end
